9f532ca4426a05a2ea13b3eeb9fe2f3d5e981547
walleenates <walleenates808@gmail.com>
2023-12-08 23:38:44 +0800

    todo

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbiCr6FlhVyRsxItHNucmYn3Gsr_HE5JM",
+  authDomain: "todoapp-86547.firebaseapp.com",
+  projectId: "todoapp-86547",
+  storageBucket: "todoapp-86547.appspot.com",
+  messagingSenderId: "145395955006",
+  appId: "1:145395955006:web:4b24a367edd85eb20e686f"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
